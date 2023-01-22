965d9553c95cf72bb943e68a7330483574114cdc
TyroneOrrego <steven.orrego93@gmail.com>
2023-01-22 15:56:29 -0500

    project in process

---
+++ client/src/App.js
@@ -0,0 +1,38 @@
+import { useEffect } from "react";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import Checkout from "./scenes/checkout/Checkout";
+import Confirmation from "./scenes/checkout/Confirmation";
+import CartMenu from "./scenes/Global/CartMenu";
+import NavBar from "./scenes/Global/NavBar";
+import Home from "./scenes/Home/Home";
+import ItemDetails from "./scenes/itemDetails/ItemDetails";
+import Footer from "./scenes/Global/Footer";
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+};
+
+function App() {
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <NavBar />
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="item/:itemId" element={<ItemDetails />} />
+          <Route path="checkout" element={<Checkout />} />
+          <Route path="checkout/success" element={<Confirmation />} />
+        </Routes>
+        <CartMenu />
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
